2ff00d8ab87563c6c82631d9d04d15dea771d058
Louis Agyapong <agyapong.louis@gmail.com>
2021-10-18 05:34:56 +0000

    custom user model, custom user manager

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model
+    """
+    email = models.EmailField(_("email address"), unique=True)
+    username = models.CharField(_("username"), max_length=30, unique=True)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    other_names = models.CharField(_("other names"), max_length=150, blank=True)
+    start_date = models.DateTimeField(_("start date"), default=timezone.now)
+    about = models.TextField(_("about"), blank=True)
+    is_staff = models.BooleanField(_("staff status"), default=False)
+    is_active = models.BooleanField(_("active"), default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+    
+    def __str__(self):
+        return self.username
+    
