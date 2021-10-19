172a32c914af39da35d860496e18369ba3415da2
Louis Agyapong <agyapong.louis@gmail.com>
2021-10-19 00:02:43 +0000

    custom user model admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    search_fields = ("email", "username", "first_name", "last_name")
+    list_display = ("email", "username", "first_name", "last_name", "is_active", "is_staff")
+    ordering = ("email", "username", "first_name", "last_name")
+    list_filter = ("email", "username", "first_name", "last_name", "is_active", "is_staff")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
