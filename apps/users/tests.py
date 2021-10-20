4caf8ec910e6f9044870fb8b9db2d232ed002116
Louis Agyapong <agyapong.louis@gmail.com>
2021-10-20 16:05:33 +0000

    tests

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email="normal@user.com",
+            username="normal",
+            first_name="Normal",
+            last_name="User",
+            password="foo",
+        )
+        self.assertEqual(user.email, "normal@user.com")
+        self.assertEqual(user.first_name, "Normal")
+        self.assertEqual(user.last_name, "User")
+        self.assertEqual(user.username, "normal")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        self.assertEqual(str(user), "normal@user.com")
+
+        with self.assertRaises(ValueError):
+            User.objects.create_user(
+                email="", password="foo", username="normal", first_name="Normal", last_name="User"
+            )
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        superuser = User.objects.create_superuser(
+            email="super@user.com", username="super", first_name="Super", last_name="User", password="foo"
+        )
+        self.assertEqual(superuser.email, "super@user.com")
+        self.assertEqual(superuser.username, "super")
+        self.assertEqual(superuser.first_name, "Super")
+        self.assertEqual(superuser.last_name, "User")
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
+        self.assertEqual(str(superuser), "super@user.com")
+
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email="super@user.com",
+                password="foo",
+                username="super",
+                first_name="Super",
+                last_name="User",
+                is_staff=False,
+            )
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email="super@user.com",
+                password="foo",
+                username="super",
+                first_name="Super",
+                last_name="User",
+                is_superuser=False,
+            )
+
+        with self.assertRaises(ValueError):
+            User.objects.create_user(
+                email="", password="foo", username="normal", first_name="Normal", last_name="User"
+            )
